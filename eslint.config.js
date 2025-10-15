bbf2da54b36af7004336379b5d11813421bc1b3e
Manav Misra <manavm@visionwebsoft.com>
2025-10-14 22:25:21 -0500

    chore: :wrench:

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,13 @@
 import eslintJs from "@eslint/js";
-import globals from "globals";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import prettier from "eslint-config-prettier";
+import globals from "globals";
 
 const STUDENT_LIMITS = {
   MAX_LINES: 30, // Forces focused functions
   MAX_PARAMS: 2, // Forces good design decisions
   MAX_DEPTH: 2, // Prevents nested spaghetti
-  MIN_NAME_LENGTH: 2, // Prevents single-char variables
   MAX_STATEMENTS: 10, // Forces decomposition
 };
 
@@ -39,25 +38,6 @@ export default [
       "max-depth": ["error", STUDENT_LIMITS.MAX_DEPTH],
       "no-nested-ternary": "error",
 
-      // Clear naming
-      "id-length": [
-        "error",
-        {
-          min: STUDENT_LIMITS.MIN_NAME_LENGTH,
-          exceptions: [
-            "i",
-            "j",
-            "k", // Loop counters
-            "x",
-            "y",
-            "z", // Coordinates
-            "e", // Event handlers
-            "n", // Common for numbers in math
-            "_", // Lodash or unused params
-            "t", // Common for time/translate functions
-          ],
-        },
-      ],
       camelcase: "error",
 
       // Modern JavaScript
@@ -91,16 +71,12 @@ export default [
       // No sloppy code
       "no-unused-vars": "error",
       "no-debugger": "error",
-      "no-alert": "error",
       "no-console": [
         "error",
         {
           allow: ["info", "warn", "error", "table", "time", "timeEnd"],
         },
       ],
-
-      // Comments
-      "spaced-comment": ["error", "always"],
     },
   },
   {
